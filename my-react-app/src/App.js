e4fd4a2e3f8f2e53fafc58588a74952114b41c2f
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-30 12:31:07 +0530

    create-react-app and vite react app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello my is mohd shoaib</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Hello Welcome to react</h1>
+    </>
+  );
+}
+
+export default App;
