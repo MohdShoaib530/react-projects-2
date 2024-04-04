5f55e1b8bea66ba4963688de827cf2861d8206ea
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-04-04 22:12:54 +0530

    context api and localStorage

---
+++ 10contextapiLocalS/src/context/todoContext.js
@@ -0,0 +1,9 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
