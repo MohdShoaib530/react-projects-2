b6a50a5058377a262e51af7f8c69eb5313a2ebfb
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-04-09 15:13:23 +0530

    redux toolkit is working

--- reduxToolkittodo/src/App.jsx
+++ reduxToolkittodo/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
-    <h1 className='text-center text-2xl text-white'>Redux toolkit learning</h1>
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
   )
 }
 
